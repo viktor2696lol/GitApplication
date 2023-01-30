b8a50ef12317640a158a65d6f5c02d916c69a1e8
федор уваркин <f.uvarkin@gmail.com>
2023-01-30 23:34:56 +0300

    Добавлена функция MatrixB

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -2,11 +2,25 @@
 
 namespace GitApplication
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MatrixB(10, 10);
+        }
+        public static void MatrixB(int width, int height)
+        {
+            Random rnd = new Random();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(rnd.Next(0, 2) + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
         }
     }
 }

d88f6c394632f488f9ef50d95a8cdaf517f17ff0
VVT <VVT@LAPTOP-6I60JO2H>
2023-01-30 23:12:59 +0300

    Добавьте файлы проекта.

---
+++ GitApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
